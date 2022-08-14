12f075a1fd1294a7d6cf51f3f5fa58d0e6e1d875
TheElemental457 <109037592+TheElemental457@users.noreply.github.com>
2022-08-15 00:19:20 +0530

    Leetcode

---
+++ Leetcode/Easy Problemset/1_TwoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int check;
+        vector<int> result(2);
+        for(int l=0; l<nums.size(); l++){
+            check=nums[l];
+            for(int i=(l+1); i<nums.size(); i++){
+                if(nums[l]+nums[i]==target){
+                    result[0]=l;
+                    result[1]=i;
+                    break;
+                }
+            }            
+        }
+        return result;
+    }
+};
