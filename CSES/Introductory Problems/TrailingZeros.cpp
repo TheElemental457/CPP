4ab0e56833dc3819a98118de3b819857d411913c
TheElemental457 <109037592+TheElemental457@users.noreply.github.com>
2022-08-31 14:00:32 +0530

    CSES

--- CSES/Introductory Problems/TrailingZeros.cpp
+++ CSES/Introductory Problems/TrailingZeros.cpp
@@ -6,7 +6,7 @@ For example 5! is 120.
 5! is 5*4*3*2*1.
 You can rewrite this as 4*3(5*2), or 12*10.
 Whenever we multiply a number by 10, it adds a zero.
-So, we have to count how many pairs of 5 and 2 there are, but we dont need to count the 2s because
+So, we have to count how many pairs of 5 and 2 there are, but we don't need to count the 2s because
 there are more pairs of 2 then 5, since there are even numbers.
 */
  
